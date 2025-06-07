c99d4898aed0277bc78cb6a68ab12aa6ebb2bbb5
trait_de_la_mer <vasya24.07@mail.ru>
2025-06-07 18:54:39 +0300

    сеттеры в самой орге

--- src/Collection/Organization.java
+++ src/Collection/Organization.java
@@ -10,9 +10,56 @@ public class Organization{
     private java.time.LocalDateTime creationDate; //Поле не может быть null, Значение этого поля должно генерироваться автоматически
     private Integer annualTurnover; //Поле может быть null, Значение поля должно быть больше 0
     private String fullName; //Длина строки не должна быть больше 1354, Поле может быть null
+    private OrganizationType type; //Поле не может быть null
+    private Address postalAddress; //Поле не может быть null
 
     public void setCreationDate(LocalDateTime creationDate) {
+        if (creationDate == null) throw new IllegalArgumentException("время создания не может быть null");
         this.creationDate = creationDate;
+    }
+
+
+    public void setId(int id) {
+        if (id <= 0) throw new IllegalArgumentException("Id должно быть > 0");
+        this.id = id;
+    }
+
+
+    public void setName(String name) {
+        if (name == null) throw new IllegalArgumentException("имя не может быть null");
+        if (name.isEmpty()) throw new IllegalArgumentException("имя не может быть пустым");
+        this.name = name;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        if (coordinates == null) throw new IllegalArgumentException("координаты не могут быть null");
+        this.coordinates = coordinates;
+    }
+
+    public void setAnnualTurnover(Integer annualTurnover) {
+        if (annualTurnover == null) throw new IllegalArgumentException("оборот не может быть null");
+        if (annualTurnover <= 0) throw new IllegalArgumentException("оборот должен быть > 0");
+        this.annualTurnover = annualTurnover;
+    }
+
+    public void setFullName(String fullName) {
+        if (fullName == null) throw new IllegalArgumentException("полное имя не может быть null");
+        if (fullName.length() > 1354) throw new IllegalArgumentException("слишком длинное!");
+        this.fullName = fullName;
+    }
+
+    public void setType(OrganizationType type) {
+        if (type == null) throw new IllegalArgumentException("тип не может быть null");
+        this.type = type;
+    }
+
+    public void setPostalAddress(Address postalAddress) {
+        if (postalAddress == null) throw new IllegalArgumentException("адрес не может быть null");
+        this.postalAddress = postalAddress;
+    }
+
+    public static Integer getCurrentId() {
+        return currentId;
     }
 
     @Override
@@ -27,44 +74,5 @@ public class Organization{
                 ", type=" + type +
                 ", postalAddress=" + postalAddress +
                 '}';
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    private OrganizationType type; //Поле не может быть null
-    private Address postalAddress; //Поле не может быть null
-
-    public static void setCurrentId(Integer currentId) {
-        Organization.currentId = currentId;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setCoordinates(Coordinates coordinates) {
-        this.coordinates = coordinates;
-    }
-
-    public void setAnnualTurnover(Integer annualTurnover) {
-        this.annualTurnover = annualTurnover;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public void setType(OrganizationType type) {
-        this.type = type;
-    }
-
-    public void setPostalAddress(Address postalAddress) {
-        this.postalAddress = postalAddress;
-    }
-
-    public static Integer getCurrentId() {
-        return currentId;
     }
 }
